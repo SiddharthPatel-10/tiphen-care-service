6a4e7a1f78205e7c1d4e5436752f7207d9c757fa
SiddharthPatel-10 <connectwithsiddh@gmail.com>
2024-06-30 03:59:58 +0530

    first commit message

---
+++ server/models/Subscription.js
@@ -0,0 +1,43 @@
+// const mongoose = require('mongoose');
+
+// const subscriptionSchema = new mongoose.Schema({
+//   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+//   product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+//   provider: { type: mongoose.Schema.Types.ObjectId, ref: 'Provider' },
+//   plan: {
+//     duration: String,
+//     frequency: String,
+//     startDate: Date,
+//     endDate: Date
+//   },
+//   address: String,
+//   status: { type: String, default: 'active' }
+// });
+
+// module.exports = mongoose.model('Subscription', subscriptionSchema);
+
+
+
+const mongoose = require('mongoose');
+
+const feedbackSchema = new mongoose.Schema({
+  date: Date,
+  feedback: String
+});
+
+const subscriptionSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+  provider: { type: mongoose.Schema.Types.ObjectId, ref: 'Provider' },
+  plan: {
+    duration: String,
+    frequency: String,
+    startDate: Date,
+    endDate: Date
+  },
+  lunches: [feedbackSchema],
+  address: String,
+  status: { type: String, default: 'active' }
+});
+
+module.exports = mongoose.model('Subscription', subscriptionSchema);
